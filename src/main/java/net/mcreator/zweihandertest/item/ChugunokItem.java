88feee01334aa878b46897aff5d36d3b62d0aec4
FGRUNN <nikietrus@gmail.com>
2024-04-03 20:25:34 +0300

    1

---
+++ src/main/java/net/mcreator/zweihandertest/item/ChugunokItem.java
@@ -0,0 +1,67 @@
+
+package net.mcreator.zweihandertest.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.world.World;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.item.UseAction;
+import net.minecraft.item.Rarity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.block.BlockState;
+
+import net.mcreator.zweihandertest.itemgroup.TestItemGroup;
+import net.mcreator.zweihandertest.ZweihanderTestModElements;
+
+import java.util.List;
+
+@ZweihanderTestModElements.ModElement.Tag
+public class ChugunokItem extends ZweihanderTestModElements.ModElement {
+	@ObjectHolder("zweihander_test:chugunok")
+	public static final Item block = null;
+
+	public ChugunokItem(ZweihanderTestModElements instance) {
+		super(instance, 7);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemCustom());
+	}
+
+	public static class ItemCustom extends Item {
+		public ItemCustom() {
+			super(new Item.Properties().group(TestItemGroup.tab).maxStackSize(8).isImmuneToFire().rarity(Rarity.COMMON));
+			setRegistryName("chugunok");
+		}
+
+		@Override
+		public UseAction getUseAction(ItemStack itemstack) {
+			return UseAction.EAT;
+		}
+
+		@Override
+		public int getItemEnchantability() {
+			return 0;
+		}
+
+		@Override
+		public int getUseDuration(ItemStack itemstack) {
+			return 0;
+		}
+
+		@Override
+		public float getDestroySpeed(ItemStack par1ItemStack, BlockState par2Block) {
+			return 1F;
+		}
+
+		@Override
+		public void addInformation(ItemStack itemstack, World world, List<ITextComponent> list, ITooltipFlag flag) {
+			super.addInformation(itemstack, world, list, flag);
+			list.add(new StringTextComponent("ZweihanderRP"));
+		}
+	}
+}
