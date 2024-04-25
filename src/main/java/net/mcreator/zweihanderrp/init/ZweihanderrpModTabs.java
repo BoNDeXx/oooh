fed5860146ba75a2cf7c7c9d8701c8e0ca4534dd
BoNDeX <fdsfdfgv@gmail.com>
2024-04-25 22:54:16 +0400

    fix

--- src/main/java/net/mcreator/zweihanderrp/init/ZweihanderrpModTabs.java
+++ src/main/java/net/mcreator/zweihanderrp/init/ZweihanderrpModTabs.java
@@ -6,20 +6,15 @@ package net.mcreator.zweihanderrp.init;
 
 import net.minecraftforge.registries.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.network.chat.Component;
 import net.minecraft.core.registries.Registries;
 
 import net.mcreator.zweihanderrp.ZweihanderrpMod;
 
-@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ZweihanderrpModTabs {
 	public static final DeferredRegister<CreativeModeTab> REGISTRY = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, ZweihanderrpMod.MODID);
 	public static final RegistryObject<CreativeModeTab> ZWEIHANDER_RP_CORE = REGISTRY.register("zweihander_rp_core",
@@ -97,6 +92,7 @@ public class ZweihanderrpModTabs {
 				tabData.accept(ZweihanderrpModItems.TEMPLARKIT_LEGGINGS.get());
 				tabData.accept(ZweihanderrpModItems.HUNGARIAN_TUNIC_LEGGINGS.get());
 				tabData.accept(ZweihanderrpModItems.ANTIOCHIAN_TUNIC_LEGGINGS.get());
+				tabData.accept(ZweihanderrpModItems.TEUTON_TUNIC_LEGGINGS.get());
 				tabData.accept(ZweihanderrpModItems.TEMPLAR_ALT_TUNIC_LEGGINGS.get());
 				tabData.accept(ZweihanderrpModItems.HOSPITALLER_TUNIC_1_LEGGINGS.get());
 				tabData.accept(ZweihanderrpModItems.HOSPITALLER_TUNIC_2_LEGGINGS.get());
@@ -111,6 +107,7 @@ public class ZweihanderrpModTabs {
 				tabData.accept(ZweihanderrpModItems.CHAINMAILSHIRTRED_LEGGINGS.get());
 				tabData.accept(ZweihanderrpModItems.CHAINMAILSHIRTDEFAULT_HELMET.get());
 				tabData.accept(ZweihanderrpModItems.CHAINMAILSHIRTDEFAULT_LEGGINGS.get());
+				tabData.accept(ZweihanderrpModItems.CHAINMAILSHIRTPURP_LEGGINGS.get());
 				tabData.accept(ZweihanderrpModItems.NORWEGIANTUNIC_LEGGINGS.get());
 				tabData.accept(ZweihanderrpModItems.SCOTTISHTUNIC_LEGGINGS.get());
 				tabData.accept(ZweihanderrpModItems.RED_TUNIC_1_LEGGINGS.get());
@@ -135,13 +132,4 @@ public class ZweihanderrpModTabs {
 				tabData.accept(ZweihanderrpModItems.SHIELD_1.get());
 				tabData.accept(ZweihanderrpModItems.BUCKLER.get());
 			}).withSearchBar().build());
-
-	@SubscribeEvent
-	public static void buildTabContentsVanilla(BuildCreativeModeTabContentsEvent tabData) {
-
-		if (tabData.getTabKey() == CreativeModeTabs.COMBAT) {
-			tabData.accept(ZweihanderrpModItems.TEUTON_TUNIC_LEGGINGS.get());
-			tabData.accept(ZweihanderrpModItems.CHAINMAILSHIRTPURP_LEGGINGS.get());
-		}
-	}
 }
